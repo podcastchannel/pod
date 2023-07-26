d4cb3e76ea5673f4fa202e64665ab743ba3f9a92
cathsuchurch <cathsuchurch@gmail.com>
2023-07-26 18:31:18 +0000

    change

--- RadioProject/asgi.py
+++ RadioProject/asgi.py
@@ -1,21 +1,6 @@
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 django_asgi_app = get_asgi_application()
-
-# import Channel_app.routing
-
-# application = ProtocolTypeRouter(
-#     {
-#         "http" : django_asgi_app,
-#         "websocket" : AllowedHostsOriginValidator(
-#             AuthMiddlewareStack(URLRouter(Channel_app.routing.websocket_urlpatterns))
-#         )
-#     }
-# )
-
