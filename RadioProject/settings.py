9dd7d928cb0c22301e07d71b910c783de3463104
cathsuchurch <cathsuchurch@gmail.com>
2023-07-26 18:17:50 +0000

    change

--- RadioProject/settings.py
+++ RadioProject/settings.py
@@ -138,11 +138,11 @@ cloudinary.config(
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'django-db'
-if CELERY_RESULT_BACKEND == 'django-db':INSTALLED_APPS += ['django_celery_results',]
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'GMT'
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+# CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+# CELERY_RESULT_BACKEND = 'django-db'
+# if CELERY_RESULT_BACKEND == 'django-db':INSTALLED_APPS += ['django_celery_results',]
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'GMT'
+# CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
